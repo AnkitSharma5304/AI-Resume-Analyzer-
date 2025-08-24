6d755319ab7756b3231c7a4ace7b215f2d408f8d
AnkitSharma5304 <ankit676756@gmail.com>
2025-08-24 22:32:39 +0530

    Added route section

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,18 +2,28 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import { resumes } from "../../constants";
 import ResumeCard from "../components/ResumeCard";
+import { useNavigate, useLocation } from "react-router"; 
+import { usePuterStore } from "~/lib/puter"
+import { useEffect } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ResumeAI" },
     { name: "description", content: "Smart Feedback for your dream job" },
   ];
-}
-
+}  
 export default function Home() {
+   const {auth } = usePuterStore();
+  const navigate = useNavigate();
+
+ useEffect(() => {
+  if (!auth.isAuthenticated) {
+    navigate('/auth?next=/');
+  }
+}, [auth.isAuthenticated]); 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
-
+ 
 <section className="main-section">
     <div className="page-heading py-16">
       <h1>Track Your Resume Ratings and Applications </h1>
